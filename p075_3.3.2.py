228c9f742c582ab30c41a1bb124d575a0ef64184
triwave33 <triwave33@gmail.com>
2019-03-31 07:31:29 +0900

    various kernels

--- p075_3.3.2.py
+++ p075_3.3.2.py
@@ -1,7 +1,11 @@
 ### -*-coding:utf-8-*-
 ## 3.3.2 Various kernels
+import matplotlib.backends
+import matplotlib 
+matplotlib.use('Agg')
 
 import numpy as np
+
 import matplotlib.pyplot as plt
 import seaborn as sns
 from scipy import stats as st
@@ -13,13 +17,13 @@ from scipy import stats as st
 def linear(x, x_dash):
     return np.array([1,x]).dot(np.array([1,x_dash]))
 # gaussian
-def gauss(x, x_dash, theta=1.0):
+def rbf(x, x_dash, theta=1.0):
     return np.exp(-1* ((x-x_dash)**2)/theta)
 # exponential kernel
 def exponential(x, x_dash, theta=1.0):
     return np.exp(-1* np.abs(x-x_dash)/theta)
 # periodic kernel
-def periodec(x, x_dash, theta1=1.0, theta2=np.pi/6.):
+def periodic(x, x_dash, theta1=1.0, theta2=np.pi/6.):
     return np.exp(theta1 * np.cos(np.abs(x - x_dash)/theta2))
 
 ## define callback function
@@ -28,18 +32,17 @@ def handler(func, *args):
 
 # set sampling point
 N = 100 # for obtaining smooth line
-callbacks = [linear, gauss, exponential,periodec]
-titles = ['linear', 'gauss', 'exponential', 'periodec']
+callbacks = {'linear':linear, 'rbf':rbf, 'exponential':exponential,'periodic':periodic}
 
 # set drawing cambus
-plt, ax = plt.subplots(2, 2, figsize=((8,6)))
-
+fig, ax = plt.subplots(2, 2, figsize=((8,6)))
 
 x = np.linspace(-4,4,N)
 
-for ind,c in enumerate(callbacks):
+for ind,key in enumerate(callbacks):
+
     # covariance matrix K
-    K = [[handler(c,i,j) for i in x for j in x]]
+    K = [[handler(callbacks[key],i,j) for i in x for j in x]]
     K = np.array(K).reshape(N,N)
 
     # sampling from n-D Gaussian distribution
@@ -48,9 +51,41 @@ for ind,c in enumerate(callbacks):
     ## draw
     for yy in y: # 4 iterations
         ax[ind//2][ind%2].plot(x,yy)
-    ax[ind//2][ind%2].set_title(titles[ind])
+    ax[ind//2][ind%2].set_title(key)
     ax[ind//2][ind%2].set_xlim(-4,4)
     ax[ind//2][ind%2].set_ylim(-4,4)
-    
-plt.savefig("p075_3.3.2.png")
 
+
+    
+fig.savefig("p075_3.3.2_sample.png")
+plt.close()
+
+fig, ax = plt.subplots(2,2, figsize=((10,8)))
+for ind,key in enumerate(callbacks):
+    # covariance matrix K
+    K = [[handler(callbacks[key],i,j) for i in x for j in x]]
+    K = np.array(K).reshape(N,N)
+    ax[ind//2][ind%2] = sns.heatmap(K, ax=ax[ind//2][ind%2])
+    ax[ind//2][ind%2].set_title(key)
+fig.savefig("p075_3.3.2_kernel.png")
+plt.close()
+
+### supplimentsl
+# create mixed kernels
+## rbf and periodic kernel
+c1= 'rbf'
+c2 = 'linear'
+fig, ax = plt.subplots(1,1, figsize=((8,8)))
+K_c1 = [[handler(callbacks[key],i,j) for i in x for j in x]]
+K_c1 = np.array(K)
+K_c2 = [[handler(callbacks[key],i,j) for i in x for j in x]]
+K_c2 = np.array(K)
+K = K_c1 + K_c2
+ax = sns.heatmap(K, ax=ax)
+ax.set_title("%s and %s kernels" % (str(c1), str(c2)))
+
+
+fig.savefig("p075_3.3.2_%s_%s_kernel.png" % (str(c1), str(c2)))
+plt.close()
+
+ 
