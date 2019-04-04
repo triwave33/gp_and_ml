912092ec1c978c1ad061329f5cf3a66bdaa0f4ad
triwave33 <triwave33@gmail.com>
2019-04-04 19:24:45 +0900

    delete comments

--- gp_regression.py
+++ gp_regression.py
@@ -6,21 +6,6 @@ import matplotlib.pyplot as plt
 from PIL import Image, ImageDraw
 import glob
 
-### functionize gp regression
-#input:
-#    x_train: 
-#    y_train:
-#    x_test: usually as a grid sample 
-##paramter:
-#    theta1
-#    theta2
-#    theta3
-#
-#output
-#    mu: predictive mean
-#    var: uncertainly of prediction (sigma**2)
-#   var_diag: diagnostic value of var matrix
-
 
 # parameter of kernel function
 theta1 = 1.0
@@ -31,16 +16,7 @@ theta3 = 0.2
 def rbf(x, x_dash,  theta1=theta1, theta2=theta2):
     return theta1 * np.exp(-1* ((x-x_dash)**2)/theta2) 
 
-# train data 
-x_train = np.array([-5, -3.,-1.8,0.8, 2.3])
-y_train = np.array([0.8, 2.1, 1.78,2.4, 1.2])
-assert len(x_train) == len(y_train)
-N = len(y_train)
-# test data
-M =100
-x_test = np.linspace (-5,3,M)
-
-
+# whole Gaussian processs
 def gp_reg(x_train, y_train, x_test, t1, t2, t3):
     N = len(x_train)
     M = len(x_test)
@@ -68,35 +44,13 @@ def gp_reg(x_train, y_train, x_test, t1, t2, t3):
     var_diag = np.diag(var) # 
     return mu, var, var_diag
 
-## plot
-#plt.scatter(x_train, y_train, marker='x') # plot of train data
-#plt.plot(x_test, mu, c='Orange') # predictive line using GP regression
-## error region (using 2 sigma)
-#plt.fill_between(x_test, mu-2 *var_diag, mu+2*var_diag, color='grey', alpha=.4)
-#plt.savefig("p086_3.4.2_GP_regression.png")
-#
-
-theta2_list = [0.5, 1., 3]
-theta3_list = [0.0, 0.1, 0.5]
 
 N = 20
 sigma = 0.3
 coef = 0.5
 x_test = np.linspace(0,7,200)
 x_sampler = np.random.rand(N) * 5 # [0,5]
-#x_sampler = np.sort(x_sampler)
 y_sampler = coef * x_sampler + np.sin(x_sampler) + np.random.normal(size=N) * sigma
-#fig, ax = plt.subplots(3,3, figsize=(12,12))
-
-#for i, t2 in enumerate(theta2_list):
-#    for j, t3 in enumerate(theta3_list):
-#        print("{}, {}".format(i,j))
-#        mu, var, var_diag = gp_reg(x_train, y_train, x_test, theta1, t2, t3)
-#
-#        ax[i][j].scatter(x_train, y_train, marker='x') # plot of train data
-#        ax[i][j].plot(x_test, mu, c='Orange') # predictive line using GP regression
-#        ax[i][j].fill_between(x_test, mu-2 *var_diag, mu+2*var_diag, color='grey', alpha=.4)
-#        ax[i][j].set_title("theta2: {}, theta3: {}".format(t2, t3))
 
 for i in range(N):
     fig = plt.figure(figsize=(4,4))
